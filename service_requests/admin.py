ae7e4c4891fe5af3535a04885d00184a91511a43
Sean Freaney <your.email@example.com>
2024-10-20 17:12:06 +0100

    added DocumentInLine and DocumentAdmin models to admin.py in service_request app

--- service_requests/admin.py
+++ service_requests/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import ServiceRequest, Document
 
 # Register your models here.
+admin.site.register(ServiceRequest)
+
+class DocumentInline(admin.TabularInline):
+    model = Document
+    extra = 1
+    fields = ('file', 'is_bank_statement', 'is_completed_file', 'uploaded_by')
+    readonly_fields = ('uploaded_at',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # If this is a new object
+            obj.uploaded_by = request.user
+        super().save_model(request, obj, form, change)
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('service_request', 'file', 'is_bank_statement', 'is_completed_file', 'uploaded_at', 'uploaded_by')
+    list_filter = ('is_bank_statement', 'is_completed_file', 'uploaded_at')
+    search_fields = ('service_request__request_number', 'file')
+    readonly_fields = ('uploaded_at',)
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:  # If this is a new object
+            obj.uploaded_by = request.user
+        super().save_model(request, obj, form, change)
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        formfield = super().formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'file':
+            formfield.help_text = 'Allowed file types: Excel (.xlsx, .xls), PDF (.pdf), Images (.jpg, .jpeg, .png)'
+        return formfield
