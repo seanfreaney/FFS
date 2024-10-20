b7d6ae493a394736bf11eafb5f5a4255bb87068d
Sean Freaney <your.email@example.com>
2024-10-20 18:11:06 +0100

    add service_request forms.py and urls.py, update root level urls.py

--- ffs/urls.py
+++ ffs/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('home.urls')),
     path('profile/', include('profiles.urls')),
+    path('service-requests/', include('service_requests.urls')),
+    path('', include('home.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
