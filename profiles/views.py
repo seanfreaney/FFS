424a42d9da3ed554eb8df35ab7a82821f9a0fd3b
Sean Freaney <your.email@example.com>
2024-11-07 20:53:17 +0000

    updated profile app views.py to include user's service requests

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from service_requests.models import ServiceRequest
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
-
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+    service_requests = ServiceRequest.objects.filter(user=request.user).order_by('-created_on')
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
@@ -20,6 +23,8 @@ def profile(request):
     template = 'profiles/profile.html'
     context = {
         'form': form,
+        'service_requests': service_requests,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
