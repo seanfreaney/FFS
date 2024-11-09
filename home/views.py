0517927a8b3978fd8243842c0e03a5a44a415caf
Sean Freaney <your.email@example.com>
2024-11-09 17:44:22 +0000

    update home views.py

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import NewsletterSubscriber
 
 # Create your views here.
 
