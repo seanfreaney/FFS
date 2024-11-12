efb39696ad0b412c26d4f55e1fa110b0e8fb100e
Sean Freaney <your.email@example.com>
2024-11-12 20:14:10 +0000

    update settings.py print statements

--- ffs/settings.py
+++ ffs/settings.py
@@ -177,6 +177,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 print("USE_AWS value:", os.environ.get('USE_AWS'))
 if os.environ.get('USE_AWS') == 'True':
+    # Bucket Config first
+    AWS_STORAGE_BUCKET_NAME = 'ffs-freaney-financial-services'
+    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Now we can safely print debug info
     print("AWS Settings:", {
         'bucket': AWS_STORAGE_BUCKET_NAME,
         'region': AWS_S3_REGION_NAME,
@@ -189,13 +197,6 @@ if os.environ.get('USE_AWS') == 'True':
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-
-    # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'ffs-freaney-financial-services'
-    AWS_S3_REGION_NAME = 'eu-north-1'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
