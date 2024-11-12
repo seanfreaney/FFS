f5af6e6728ec7ddd2c1c84d125274cfb382a8b97
Sean Freaney <your.email@example.com>
2024-11-12 07:52:29 +0000

    ad debug statements

--- custom_storages.py
+++ custom_storages.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 print("Loading custom_storages.py")
+print(f"STATICFILES_LOCATION: {settings.STATICFILES_LOCATION}")
+print(f"STATIC_URL: {settings.STATIC_URL}")
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
+    print(f"StaticStorage location: {location}")
 
 
 class MediaStorage(S3Boto3Storage):
