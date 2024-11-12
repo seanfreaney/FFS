cade4ae861b8bc2b80aee30ba2b54b19fa937524
Sean Freaney <your.email@example.com>
2024-11-12 21:56:29 +0000

    update settings.py

--- ffs/settings.py
+++ ffs/settings.py
@@ -135,10 +135,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
