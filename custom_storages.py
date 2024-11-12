4d7673867e619b48fd7ba40276ea1418e072ddda
Sean Freaney <your.email@example.com>
2024-11-12 20:17:19 +0000

    update print statemnts in customer storages

--- custom_storages.py
+++ custom_storages.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+import logging
 
-print("Loading custom_storages.py")
-print(f"STATICFILES_LOCATION: {settings.STATICFILES_LOCATION}")
-print(f"STATIC_URL: {settings.STATIC_URL}")
+logger = logging.getLogger(__name__)
+
+logger.error("Loading custom_storages.py")
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
-    print(f"StaticStorage location: {location}")
-
+    logger.error(f"StaticStorage initialized with location: {location}")
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
+    logger.error(f"MediaStorage initialized with location: {location}")
