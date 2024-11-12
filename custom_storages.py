3f614afab237beb9749c33e7196166c37f510695
Sean Freaney <your.email@example.com>
2024-11-12 20:51:24 +0000

    update custom storages

--- custom_storages.py
+++ custom_storages.py
@@ -1,17 +1,16 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
-import logging
-
-logger = logging.getLogger(__name__)
-
-logger.error("Loading custom_storages.py")
 
 class StaticStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
     default_acl = 'public-read'
-    logger.error(f"StaticStorage initialized with location: {location}")
+    file_overwrite = True
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
 
 class MediaStorage(S3Boto3Storage):
     location = settings.MEDIAFILES_LOCATION
     default_acl = 'public-read'
-    logger.error(f"MediaStorage initialized with location: {location}")
+    file_overwrite = True
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    custom_domain = settings.AWS_S3_CUSTOM_DOMAIN
